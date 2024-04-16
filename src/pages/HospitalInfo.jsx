3a490916af909ef21d3bcd8ce0979fef0471a991
shreyaskamble1612 <shreyaskamble393@gmail.com>
2024-04-17 01:07:02 +0530

    New

--- src/pages/HospitalInfo.jsx
+++ src/pages/HospitalInfo.jsx
@@ -3,16 +3,16 @@ import Hospital from "../assets/Hospital.png";
 const HospitalInfo = () => {
   return (
     <div>
-      <div className="flex flex-row mt-5 m-auto w-4/5 border-2 h-20 shadow-sm">
-        <img src={Hospital} alt="" className="w-9 h-9 m-3" />
-        <div className="flex flex-col">
-          <span className="text-2xl">Nayantara Hospital</span>
-          <span>Mangalwar peth,Kolhapur</span>
+      <div className="flex flex-row mt-5 m-auto w-4/5 border-2 shadow-sm space-x-4">
+        <img src={Hospital} alt="" className="w-24 h-24 m-3" />
+        <div className="flex items-center">
+          <div className="flex flex-col -mt-6">
+            <span className="text-2xl">Nayantara Hospital</span>
+            <span>Mangalwar peth,Kolhapur</span>
+          </div>
         </div>
       </div>
-      <div>
-        
-      </div>
+      <div></div>
     </div>
   );
 };
